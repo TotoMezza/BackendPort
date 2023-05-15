798588e1d8afecd5305cbe6d26cfb552eeff0061
Tomás Mezzabotta <tomasmezzabotta06@gmail.com>
2023-05-15 19:25:17 -0300

    Arreglo de Errores

--- src/main/java/com/portfolio/tm/Controller/CEducacion.java
+++ src/main/java/com/portfolio/tm/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {" https://frontendtm-eeae6.web.app"})
+@CrossOrigin(origins = {" https://frontendtm-eeae6.web.app "})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
